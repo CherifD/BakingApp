d0bdd9c98de7fda6ddf1bc92310d2c5b9fd07d57
cherifD <cherifdmc@gmail.com>
2018-11-04 16:49:50 -0500

    Added custom launcher icon, used Picasso to retrieve recipe image, and changed recipe item layout to use ConstraintLayout.

--- app/src/main/java/com/cherifcodes/bakingapp/adaptersAndListeners/RecipeAdapter.java
+++ app/src/main/java/com/cherifcodes/bakingapp/adaptersAndListeners/RecipeAdapter.java
@@ -1,15 +1,19 @@
 package com.cherifcodes.bakingapp.adaptersAndListeners;
 
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cherifcodes.bakingapp.R;
 import com.cherifcodes.bakingapp.model.Recipe;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,7 @@ import java.util.List;
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
     private List<Recipe> mRecipeList;
     private RecipeClickListener mRecipeClickListener;
+    private Context mContext;
 
     public RecipeAdapter(RecipeClickListener recipeClickListener) {
         mRecipeClickListener = recipeClickListener;
@@ -30,7 +35,8 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        mContext = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
         View view = layoutInflater.inflate(R.layout.recipe_item, parent, false);
         return new RecipeViewHolder(view);
     }
@@ -39,7 +45,16 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
         Recipe recipe = mRecipeList.get(position);
         holder.recipeNameTv.setText(recipe.getRecipeName());
-        holder.servingSizeTv.setText(recipe.getServingSize() + "");
+        holder.servingSizeTv.setText(recipe.getServingSize());
+
+        // Use Picasso to load the recipe image
+        try {
+            Picasso.with(mContext)
+                    .load(recipe.getImageUrlStr())
+                    .into(holder.recipeImageImv);
+        } catch (Exception e) {
+            Log.i(this.getClass().getSimpleName(), "Invalid recipe image url.");
+        }
     }
 
     @Override
@@ -50,11 +65,14 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView recipeNameTv;
         TextView servingSizeTv;
+        ImageView recipeImageImv;
+
 
         public RecipeViewHolder(View itemView) {
             super(itemView);
             recipeNameTv = itemView.findViewById(R.id.tv_recipeName);
             servingSizeTv = itemView.findViewById(R.id.tv_servingSize);
+            recipeImageImv = itemView.findViewById(R.id.imv_recipeImage);
             itemView.setOnClickListener(this);
         }
 
